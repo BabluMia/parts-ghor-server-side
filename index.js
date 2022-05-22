c9a22e04ee1831671bfd550533aaf16f2b45d003
Bablu Mia <bablumiah2000@gmail.com>
2022-05-23 01:05:40 +0600

    jwt added

--- index.js
+++ index.js
@@ -10,39 +10,76 @@ require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.USER_PASS}@cluster0.uxvra.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-
-
-
-
-async function run (){
-    try{
-      await client.connect();
-      const productCollection = client.db("PartsGhor").collection("products");
-      console.log("connect to dbDoctor");
-
-      app.get('/products' , async (req,res)=>{
-        const query = {};
-        const cursor = productCollection.find(query);
-        const products = await cursor.toArray();
-        res.send(products);
-      })
-      app.get("/product/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const product = await productCollection.findOne(query);
-        res.send(product);
-      });
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
     }
-    finally{
-
-    }
+    req.decoded = decoded;
+    next();
+  });
 }
 
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("PartsGhor").collection("products");
+    const userCollection = client.db("PartsGhor").collection("users");
+    const reviewCollection = client.db("PartsGhor").collection("reviews");
+    const profileCollection = client.db("PartsGhor").collection("profile");
+    console.log("connect to dbDoctor");
 
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
+
+    // get  all user
+    app.get("/user", verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+    // user collection
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const displayName = req.params.displayName
+      const user = req.body;
+      const filter = { email: email ,displayName:displayName};
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "24h" }
+      );
+      res.send({ result, token });
+    });
+  } finally {
+  }
+}
 
 // call run function
 run().catch(console.dir);
