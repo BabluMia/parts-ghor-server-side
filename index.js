e06bc2c19ea4ea67ea11787d64caef711126122e
Bablu Mia <bablumiah2000@gmail.com>
2022-05-26 11:03:44 +0600

    redmi added

--- index.js
+++ index.js
@@ -168,6 +168,7 @@ async function run() {
         return res.status(403).send({ message: "Forbidden access" });
       }
     });
+    // order delete api
     app.delete("/orders/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
